a7a45030a89504d83ea85e1656bc7a24d705a888
Drennann <delpan.drennan@gmail.com>
2022-12-21 10:52:47 -0300

    adding react native elements UI

--- financialapp/App.tsx
+++ financialapp/App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from '@rneui/base';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola!!!</Text>
+      <Button title="Hello World!"/>
       <StatusBar style="auto" />
     </View>
   );
