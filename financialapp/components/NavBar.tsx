4bf6b97849b6435a66b8dedf2dfc0ad3802c00e1
Drennann <delpan.drennan@gmail.com>
2022-12-21 21:09:49 -0300

    added navBar few styles

--- financialapp/components/NavBar.tsx
+++ financialapp/components/NavBar.tsx
@@ -3,33 +3,46 @@ import { useEffect, useState } from "react";
 import { StyleSheet, View } from "react-native";
 import { ButtonGroup } from "@rneui/base";
 
-const buttons = ['Friends', 'Transactions', 'Profile'];
+const buttons = ["Friends", "Transactions", "Profile"];
 
-export default function NavBar({navigation, route}: any) {
-  
-  const {tabName} = route.params? route.params : 0;
-  const [selectedIndex, setSelectedIndex] = useState( tabName? tabName : 0);
+export default function NavBar({ navigation, route }: any) {
+  const { tabName } = route.params ? route.params : 0;
+  const [selectedIndex, setSelectedIndex] = useState(tabName ? tabName : 0);
 
-  useEffect(()=>{
-    setSelectedIndex(tabName? tabName : 0)
-  },[])
-  
+  useEffect(() => {
+    setSelectedIndex(tabName ? tabName : 0);
+  }, []);
+
   return (
     <View>
       <ButtonGroup
-      buttons={buttons}
-      selectedIndex={selectedIndex}
-      onPress={(value) => {
-        navigation.navigate(buttons[value], {tabName: value})
-      }}
-      containerStyle={{ marginBottom: 20 }}
-    />
+        buttons={buttons}
+        selectedIndex={selectedIndex}
+        onPress={(value) => {
+          navigation.navigate(buttons[value], { tabName: value });
+        }}
+        containerStyle={styles.containerStyle}
+        textStyle={styles.navBarFont}
+        selectedButtonStyle={styles.selectedButton}
+        selectedTextStyle={{}}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   navBarFont: {
-    fontSize: 10
+    fontSize: 12,
+    color: "#d0d0d0",
+  },
+  selectedButton: {
+    backgroundColor: "#43414c",
+    borderColor: "#414141",
+  },
+  containerStyle: {
+    marginBottom: 20,
+    backgroundColor: "#23212c",
+    borderWidth: 0.5,
+    borderColor: "#d0d0d0",
   },
 });
